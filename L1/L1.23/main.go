a2422a38f09bdd76ebee9f95d2d4b60562116254
O4EHbKPYTON <yan.matveyev@outlook.com>
2025-11-12 23:28:27 +0300

    feat: completed L1.23

--- L1/L1.23/main.go
+++ L1/L1.23/main.go
@@ -1,1 +1,18 @@
-package L1_23
+package main
+
+import "fmt"
+
+func deleteIthElemFromSlice(i int, slice []int) []int {
+	if i < 0 || i >= len(slice) {
+		return slice
+	}
+
+	res := make([]int, len(slice)-1)
+	copy(res, append(slice[:i], slice[i+1:]...))
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Print(deleteIthElemFromSlice(2, nums))
+}
